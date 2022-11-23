414373461ea1b0e5ccf7dd1ae8744367d1cee845
ryan605 <langatryan26@gmail.com>
2022-11-23 12:56:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = `Ryan Langat`;
+
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
+
+const WelcomeMessage=`Welcome to Flatbook,${currentUser}!`
+
+const excitedWelcomeMessage=welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
